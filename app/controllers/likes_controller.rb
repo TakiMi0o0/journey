df7ba335f83ed046eebe2f6016580e6bf69bbbe4
TakiMi0o0 <takimiworks@gmail.com>
2024-02-05 20:25:42 +0900

    非同期まであと一歩

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,14 @@
 class LikesController < ApplicationController
-  before_action :set_book
 
   def create
-    like = current_user.likes.build(book_id: params[:book_id])
-    like.save
-    respond_to do |format|
-      format.js
-    end
+    @book = Book.find(params[:book_id])
+    current_user.likes.create(book: @book)
+    render turbo_stream: turbo_stream.append(@book, partial: "likes/like", locals: { book: @book })
   end
 
   def destroy
-    like = Like.find_by(book_id: params[:book_id], user_id: current_user.id)
-    like.destroy
-    respond_to do |format|
-      format.js
-    end
-  end
-
-  def set_book
     @book = Book.find(params[:book_id])
+    Like.find_by(book: @book, user: current_user).destroy
+    render turbo_stream: turbo_stream.remove(@book)
   end
-  
 end
