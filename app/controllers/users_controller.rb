b25ab327024d96e1a94a3c20ff90b35d0afb0277
TakiMi0o0 <takimiworks@gmail.com>
2024-02-21 19:06:42 +0900

    my journeyリンク

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @nickname = current_user.nickname
+    @books = current_user.books
+  end
+  
   def show
     @nickname = current_user.nickname
     @books = current_user.books
